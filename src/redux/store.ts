115353847e6e7bcc7700ac576428f29ac7ef309d
MOSHFIQS <nibir3211@gmail.com>
2025-08-30 00:42:54 +0600

    added the adminApi.ts and get data on the allAdmins component

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,11 +2,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import authReducer from "./features/authSlice"
+import { apiSlice } from './api/apiSlice'
 
 export const store = configureStore({
     reducer: {
-        auth : authReducer
+        auth : authReducer,
+        [apiSlice.reducerPath] : apiSlice.reducer
     },
+    middleware:(getDefaultMiddleware) => 
+        getDefaultMiddleware().concat(apiSlice.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
