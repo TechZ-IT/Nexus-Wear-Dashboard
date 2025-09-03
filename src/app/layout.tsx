4b06a4512a93e516d5f45c8cd1985b42828bfdeb
MOSHFIQS <nibir3211@gmail.com>
2025-09-03 17:58:43 +0600

    added Admin Protected dashboard functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Roboto } from "next/font/google";
 import "./globals.css";
 import AdminLayout from "./AdminLayout";
+import ReduxProvider from "@/redux/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
@@ -19,9 +20,11 @@ export default function RootLayout({
       return (
             <html lang="en">
                   <body className={`${inter.className} ${roboto.className} antialiased`}>
-                        <AdminLayout>
-                              {children}
-                        </AdminLayout>
+                        <ReduxProvider>
+                              <AdminLayout>
+                                    {children}
+                              </AdminLayout>
+                        </ReduxProvider>
                   </body>
             </html>
       );
