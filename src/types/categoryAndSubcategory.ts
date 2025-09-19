1459fc8bfe4ce0d8aa4cc274a25329a6b162b09d
MOSHFIQS <nibir3211@gmail.com>
2025-09-19 12:49:54 +0600

    added the category and subCategory types

---
+++ src/types/categoryAndSubcategory.ts
@@ -0,0 +1,30 @@
+// Category type
+export type Category = {
+     id: string;
+     deletedAt: string | null;
+     createdAt: string;
+     updatedAt: string;
+     name: string;
+     description: string;
+     image: string | null;
+}
+
+export type Subcategory = {
+     id: string;
+     deletedAt: string | null;
+     createdAt: string;
+     updatedAt: string;
+     name: string;
+     description: string;
+     image: string | null;
+     categoryId: string;
+     category: Category;
+}
+
+
+export type SubcategoryResponse = {
+     data: Subcategory[];
+     limit: number;
+     page: number;
+     total: number;
+}
