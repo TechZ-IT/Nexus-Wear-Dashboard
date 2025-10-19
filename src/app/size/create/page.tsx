f25b5d3ff3118c00d6849c7b833e69ae3ebeb56c
MOSHFIQS <nibir3211@gmail.com>
2025-10-20 04:46:07 +0600

    create skeleton for size from color

---
+++ src/app/size/create/page.tsx
@@ -0,0 +1,13 @@
+
+import ColorForm from '@/components/color/ColorForm';
+import React from 'react';
+
+const Page = () => {
+     return (
+          <div>
+               <ColorForm />
+          </div>
+     );
+};
+
+export default Page;
